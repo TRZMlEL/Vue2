28f53567aa4949dcd1ff49384ae4828bef500eae
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-09-22 20:08:11 +0200

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	new file:   src/App.vue 	new file:   src/assets/vue.svg 	new file:   src/index.css 	new file:   src/main.js 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="flex items-center justify-center w-screen h-screen bg-neutral-900 flex-col gap-6">
+
+    <h1 v-if="likes > 0" class="text-green-700 text-2xl">
+      This website have {{ likes }} 👍
+    </h1>
+
+    <h1 v-else="likes > 0" class="text-green-700 text-2xl">
+      No body like it 😢
+    </h1>
+
+    <button v-on:click="like()" class="bg-green-700 rounded-lg w-30 h-10 shadow-lg p-3 flex items-center justify-center">
+      I Like it 👍
+    </button>
+  </main>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return{
+        likes: 0,
+      }
+    },
+
+    methods: {
+      like() {
+        this.likes++;
+      }
+    }
+  }
+</script>
